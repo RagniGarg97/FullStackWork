772987e9d0e11bff245c699d9e0986b096cb1676
RagniGarg97 <54269555+RagniGarg97@users.noreply.github.com>
2019-11-22 10:21:06 +0530

    Add files via upload

---
+++ examples/core-java-examples/src/com/examples/java/concurrency/SimpleThreads.java
@@ -0,0 +1,87 @@
+package com.examples.java.concurrency;
+
+/**
+ * The MessageLoop thread prints out a series of messages.
+ * If interrupted before it has printed all its messages,
+ * the MessageLoop thread prints a message and exits.
+ */
+public class SimpleThreads {
+
+	// Display a message, preceded by
+	// the name of the current thread
+	static void threadMessage(String message) {
+		String threadName = Thread.currentThread().getName();
+		System.out.format("%s: %s%n", threadName, message);
+	}
+
+	private static class MessageLoop implements Runnable {
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			String importantInfo[] = { 
+					"Mares eat oats", 
+					"Does eat oats", 
+					"Little lambs eat ivy",
+					"A kid will eat ivy too" 
+					};
+			try {
+				for (int i = 0; i < importantInfo.length; i++) {
+					// Pause for 4 seconds
+					System.out.println("Sleeping...");
+					Thread.sleep(20000);
+					// Print a message
+					threadMessage(importantInfo[i]);
+				}
+			} catch (InterruptedException e) {
+				threadMessage("I wasn't done!");
+			}
+			long endTime = System.currentTimeMillis();
+			System.out.println("MessageLoop thread took " + (endTime - startTime)/1000 + " secs");
+		}
+	}
+
+	public static void main(String args[]) throws InterruptedException {
+
+		// Delay, in milliseconds before
+		// we interrupt MessageLoop
+		// thread (default ten seconds).
+		long patience = 1000 * 10;
+
+		// If command line argument
+		// present, gives patience
+		// in seconds.
+//		if (args.length > 0) {
+//			try {
+//				patience = Long.parseLong(args[0]) * 1000;
+//			} catch (NumberFormatException e) {
+//				System.err.println("Argument must be an integer.");
+//				System.exit(1);
+//			}
+//		}
+
+		threadMessage("Starting MessageLoop thread");
+		long startTime = System.currentTimeMillis();
+		Thread t = new Thread(new MessageLoop());
+		t.start();
+		
+		
+
+		threadMessage("Waiting for MessageLoop thread to finish");
+		// loop until MessageLoop
+		// thread exits
+		while (t.isAlive()) {
+			threadMessage("Still waiting...");
+			// Wait maximum of 1 second
+			// for MessageLoop thread
+			// to finish.
+			t.join(1000);
+			if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
+				threadMessage("Tired of waiting!");
+				t.interrupt();
+				// Shouldn't be long now
+				// -- wait indefinitely
+				t.join();
+			}
+		}
+		threadMessage("Finally!");
+	}
+}
